8a7853b98219481fb2e0c24fa72068bf7a788ebf
Eduardo Nogueira <132234306+Nogueira237@users.noreply.github.com>
2023-07-21 11:29:03 -0300

    Add files via upload

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 19
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou o limite de velocidade!`)
+}else{
+    console.log(`Continue dirijindo com cuidado`)
+}
